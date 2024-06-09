84f403b393b5042da009573f387cbb9aaa41c24e
brianling <brianling@qq.com>
2024-06-10 01:24:22 +0800

    fix(vueform.config.ts): change the form-data-transform-format to application/json

--- vueform.config.ts
+++ vueform.config.ts
@@ -4,11 +4,15 @@ import en from '@vueform/vueform/locales/en'
 import zh_CN from '@vueform/vueform/locales/zh_CN'
 import vueform from '@vueform/vueform/dist/vueform'
 import { defineConfig } from '@vueform/vueform'
+import axios from 'axios'
 
 // You might place these anywhere else in your project
 import '@vueform/vueform/dist/vueform.css';
-
+axios.defaults.headers.post = {
+    'Content-Type': 'application/json'
+  }
 export default defineConfig({
+  axios,
   theme: vueform,
   locales: { zh_CN,en },
   locale: 'zh_CN',
